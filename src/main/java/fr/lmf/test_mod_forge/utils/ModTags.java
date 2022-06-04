5cfa2509d5994c1221499fa5737bc31f7bd7eb47
Mysterious_Dev <mysteriousdevs@gmail.com>
2022-06-04 19:23:33 +0200

    Change tag biome register

--- src/main/java/fr/lmf/test_mod_forge/utils/ModTags.java
+++ src/main/java/fr/lmf/test_mod_forge/utils/ModTags.java
@@ -1,13 +1,13 @@
 package fr.lmf.test_mod_forge.utils;
 
 import fr.lmf.test_mod_forge.Main;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModTags {
 
@@ -15,10 +15,6 @@ public class ModTags {
 
     public static final TagKey<Block> TEST_TOOL_TYPE_TAG = BlockTags.create(new ResourceLocation(Main.MODID, "test_tool_type"));
 
-    public static final TagKey<Biome> HAS_ABANDONNED_HOUSE = create("has_abandonned_house");
-
-    private static TagKey<Biome> create(String p_207631_) {
-        return TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(Main.MODID, p_207631_));
-    }
+    public static final TagKey<Biome> HAS_ABANDONNED_HOUSE = DeferredRegister.create(ForgeRegistries.BIOMES, Main.MODID).createTagKey("has_abandonned_house");
 
 }
